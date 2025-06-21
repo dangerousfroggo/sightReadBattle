62ae211a769311c9768ac2cde65d85bb1a6c293f
Sergei <serg.borischev2017@gmail.com>
2025-06-21 11:31:30 -0400

    updated pitch detector

---
+++ main.py
@@ -0,0 +1,9 @@
+import audio_pitch_extractor
+import pitchMatching
+import noteToEvent
+import time
+import threading
+import queue
+import json
+import requests
+
--- noteToEvent.py
+++ noteToEvent.py
@@ -1,5 +1,18 @@
 from music21 import *
 def xmlToEvent(xmlFile, bpm = 60):
+    """
+    Converts a music21 stream (parsed from an xml file) into a list of events
+    suitable for use in pitchMatching.py. Each event is a dictionary with
+    the following keys:
+
+    - time: the time, in milliseconds, at which the event starts
+    - duration: the length, in milliseconds, of the event
+    - pitch: the frequency of the note (0 if the event is a rest)
+
+    :param xmlFile: the path to an xml file containing a music21 stream
+    :param bpm: the tempo of the piece, in beats per minute
+    :return: a list of events
+    """
     score = converter.parse(xmlFile) #lowkey no idea what tf a converter is but basically this parses it 
     events = []
     flat_score = score.parts[0].flat.notesAndRests #flattens score to series of notes and rests
@@ -7,7 +20,7 @@ def xmlToEvent(xmlFile, bpm = 60):
     print(flat_score)
     for n in flat_score:
         if isinstance(n, note.Note):
-            pitch = n.pitch.frequency
+            pitch = round(n.pitch.frequency, 2) #rounding to 2 decimal places for comparisonn.pitch.frequency
         elif isinstance(n,note.Rest):
             pitch = 0
         elif isinstance(n,note.Chord):
